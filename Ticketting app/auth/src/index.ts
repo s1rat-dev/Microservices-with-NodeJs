d8a951e23f9a00ba731b0704afc54ec62e70cd6c
s1rat-dev <siratsemih@gmail.com>
2022-03-16 13:47:55 +0300

    Ticketting app starting

---
+++ Ticketting app/auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
